4fcf12507b225660d1cfbe7891dc4c7ae907c144
shubham singh rajput <rajputshubham7496@gmail.com>
2023-11-27 03:45:17 +0530

    backend

--- src/main/java/com/codewithshubham/fullstackbackend1/controller/Usercontroller.java
+++ src/main/java/com/codewithshubham/fullstackbackend1/controller/Usercontroller.java
@@ -3,6 +3,7 @@ package com.codewithshubham.fullstackbackend1.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.codewithshubham.fullstackbackend1.exception.UserNotFoundException;
 import com.codewithshubham.fullstackbackend1.model.User1;
 import com.codewithshubham.fullstackbackend1.repository.UserRepository;
 
-@RestController
+@Controller
 @CrossOrigin("http://localhost:3000")
 public class Usercontroller {
 	
