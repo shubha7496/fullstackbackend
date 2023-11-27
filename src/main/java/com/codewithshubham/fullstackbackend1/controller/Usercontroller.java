e600d041944e052a7f51e1bacf2090b189c4dbfd
shubham singh rajput <rajputshubham7496@gmail.com>
2023-11-28 01:23:18 +0530

    main

--- src/main/java/com/codewithshubham/fullstackbackend1/controller/Usercontroller.java
+++ src/main/java/com/codewithshubham/fullstackbackend1/controller/Usercontroller.java
@@ -3,7 +3,6 @@ package com.codewithshubham.fullstackbackend1.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +19,7 @@ import com.codewithshubham.fullstackbackend1.repository.UserRepository;
 
 @RestController
 @RequestMapping("/full")
-@CrossOrigin
+@CrossOrigin("http://localhost:3000")
 public class Usercontroller {
 	
 	@Autowired
